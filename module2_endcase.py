c223e0f308ef4d194a8469cfc54997000cb492fc
RHA1705 <garbarzroman47@gmail.com>
2023-10-20 17:22:14 +0200

    add_try1

--- module2_endcase.py
+++ module2_endcase.py
@@ -3,5 +3,12 @@ operand = None
 operator = None
 wait_for_number = True
 
-while True:
-    operand = int(input('Enter a number: '))
+while wait_for_number:
+    
+    try:
+        operand = int(input('Enter a number: '))
+    except ValueError:
+        print(f'{operand} is not a number. Try again.')
+    else:
+        result += operand
+        
