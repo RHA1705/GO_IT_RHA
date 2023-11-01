3cfb39c6903234fc6ce03220e8571e84aec3c8f1
RHA1705 <garbarzroman47@gmail.com>
2023-11-01 22:49:57 +0100

    avg

--- module4.py
+++ module4.py
@@ -1,4 +1,11 @@
-points = [2, 2, 5, 3, 3, 5, 4, 5, 4, 3, 2, 3, 5, 4, 4, 5]
+grade = [2, 2, 5, 3, 3, 5, 4, 5, 4, 3, 2, 3, 5, 4, 4, 5]
 
 def split_list(grade):
+    suma = 0
+    for i in grade:
+        suma += i
     
+    avg = suma/len(grade)
+    return avg
+
+print(split_list([2, 2, 5, 3, 3, 5, 4, 5, 4, 3, 2, 3, 5, 4, 4, 5]))
