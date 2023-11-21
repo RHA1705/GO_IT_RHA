38bd7b3a0b2043c1ca3554dd5827c2da5d59cf20
RHA1705 <garbarzroman47@gmail.com>
2023-11-22 00:40:54 +0100

    new

--- module5.py
+++ module5.py
@@ -1,9 +1,13 @@
 import re
-def find_all_phones(text):
-    result = re.findall(r"(\+380\(\d{2}\)\d{3}\-\d-\d{3}|\+380\(\d{2}\)\d{3}\-\d{2}-\d{2})", text)
+
+def find_all_links(text):
+    result = []
+    iterator = re.finditer(r"(http[s]://\w+\.\w+\.\w{3})", text)
+    for match in iterator:
+        result.append(match.group())
     return result
 
-print(find_all_phones('Irma +380(67)777-7-771 second +380(67)777-77-77 aloha a@test.com abc111@test.com.net +380(67)111-777-777+380(67)777-77-787'))
+print(find_all_links('The main search site in the world is https://www.google.com The main social network for people in the world is https://www.facebook.com But programmers have their own social network http://github.com There they share their code. some url to check https://www..facebook.com www.facebook.com '))
 
 # https://bulldogjob.pl/readme/samouczek-regex-sciagawka-z-przykladami
 # https://docs.python.org/3/library/re.html
