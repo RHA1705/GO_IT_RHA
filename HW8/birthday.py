7d57e1e6ae74d5a8c56e0bf20b9ddf269304fdbc
RHA1705 <garbarzroman47@gmail.com>
2023-12-28 04:32:42 +0100

    new

---
+++ HW8/birthday.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+
+users = {
+    {'name': 'Olenka Harbazh',
+     'birthday': datetime(1995, 3, 29).date()},
+    {'name': 'Sonia Harbazh',
+     'birthday': datetime(2022, 2, 24).date()},
+    {'name': 'Roman Harbazh',
+     'birthday': datetime(1995, 5, 17)}
+}
+
+def main():
+    birthday_date = []
+
+if __name__ == "__main__":
+    main()
