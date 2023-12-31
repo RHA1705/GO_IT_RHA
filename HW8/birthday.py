3488721d0562c4697416afe36390b2091cee445c
RHA1705 <garbarzroman47@gmail.com>
2023-12-31 20:53:51 +0100

    HW8

--- HW8/birthday.py
+++ HW8/birthday.py
@@ -1,24 +1,34 @@
 from datetime import date, datetime
 
 
-def get_birthdays_per_week():
+def get_birthdays_per_week(users):
     # Реалізуйте тут домашнє завдання
-    # for user in users:
-    #     current_day = date.today().strftime("%A")
-    current_day = date.today().strftime("%A") + 1
-    print(current_day)
+    for user in users:
+        
+        user_name = user['name'].split()[0]
+        weekday_birth = user['birthday'].strftime('%A')
+        users = {}
+        if weekday_birth not in users:
+            users[weekday_birth] = []
+        users[weekday_birth].append(user_name)
+        
+    first_day = datetime(year=2023, month=12, day=31)
+    last_day = (6 - first_day.weekday()) + first_day.weekday()
+    
+    
+    print(first_day, last_day)
     # return users
 
 
 if __name__ == "__main__":
-    # users = [
-    #     {"name": "Jan Koum", 
-    #      "birthday": datetime(1976, 1, 1).date()},
-    # ]
+    users = [
+        {"name": "Jan Koum", 
+         "birthday": datetime(1976, 1, 1).date()},
+    ]
 
     # result = get_birthdays_per_week(users)
     # print(result)
     # # Виводимо результат
     # for day_name, names in result.items():
     #     print(f"{day_name}: {', '.join(names)}")
-    get_birthdays_per_week()
+    get_birthdays_per_week(users)
--- HW8/main.py
+++ HW8/main.py
@@ -1,10 +1,16 @@
-words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
-grouped_words = {}
+from datetime import date, datetime
+# 
+# words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
+# grouped_words = {}
 
-for word in words:
-    char = word[0]
-    if char not in grouped_words:
-        grouped_words[char] = []
-    grouped_words[char].append(word)
+# for word in words:
+#     char = word[0]
+#     if char not in grouped_words:
+#         grouped_words[char] = []
+#     grouped_words[char].append(word)
 
-print(grouped_words)
+# print(grouped_words)
+
+today = datetime(year=2023, month=6, day=1)
+future_year = today.month / 12
+print(int(future_year))
