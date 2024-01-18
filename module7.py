36e2b39c79c64ad8d939c7823d93950bc949a275
RHA1705 <garbarzroman47@gmail.com>
2024-01-18 13:29:31 +0100

    new

--- module7.py
+++ module7.py
@@ -18,17 +18,42 @@
 # print(capital_text('    asdf. asd! asd? asd'))
 
 '''Task Nr 6'''
-def solve_riddle(riddle, word_length, start_letter, reverse=False):
-# 1. Znaleźć start_letter in riddle.
-# 2. Odliczyć word_lenght od riddle[start_letter]
-# 4. Zwrócić wynik
-    if reverse is False:
-        x = riddle.index(start_letter)
-        result = riddle[x:x+word_length]
-        return result
-    if reverse is True:
-        x = riddle.index(start_letter)
-        result = riddle[x:x-word_length:-1]
-        return result
-    return []
-print(solve_riddle('mi1powerpret', 5, 'p'))
+# def solve_riddle(riddle, word_length, start_letter, reverse=False):
+#     try:
+#         if reverse is False:
+#             x = riddle.index(start_letter)
+#             result = riddle[x:x+word_length]
+#             return result
+#         if reverse is True:
+#             x = riddle.index(start_letter)
+#             result = riddle[x:x-word_length:-1]
+#             return result
+#     except ValueError:
+#         return ''
+# print(solve_riddle('mi1powerpret', 5, 'p'))
+
+'''Task Nr 7'''
+# def data_preparation(list_data):
+#     result = []
+#     for l in list_data:
+#         if len(l) > 0:
+#             l_max = max(l)
+#             l_min = min(l)
+#             if len(l) > 2:
+#                 l.remove(l_max)
+#                 l.remove(l_min)
+#             for i in l:
+#                 result.append(i)
+#     result.sort(reverse=True)
+#     return result
+
+# print(data_preparation([[1,2,3], [3,4], [5,6]]))
+
+'''Task Nr 8'''
+import re
+
+def token_parser(s):
+    token = re.findall(r'\d+|\+|\-|\/|\*|\(|\)', s)
+    return token
+
+print(token_parser('123 + asde -  12345'))
