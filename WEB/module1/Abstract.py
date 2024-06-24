7eae51fd19c4bda3376c3ae1df19200b6fc060a7
RHA1705 <garbarzroman47@gmail.com>
2024-06-24 18:39:00 +0200

    new

---
+++ WEB/module1/Abstract.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod, ABCMeta
+#
+#
+# class Abs:
+#     def pay(self):
+#         raise NotImplementedError
+#
+# class System(Abs):
+#     pass
+#
+#
+# class AbsAbstract(metaclass=ABCMeta):
+#     @abstractmethod
+#     def pay(self):
+#         pass
+#
+# class SystemAbs(AbsAbstract):
+#     pass
+#
+# if __name__ == '__main__':
+#     p = System()
+#     p1 = SystemAbs()
+
+class CommunicationDevice
+
--- WEB/module1/main.py
+++ WEB/module1/main.py
@@ -1,2 +1,26 @@
-class Conection:
-    
+from functools import wraps
+
+
+def greeting(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print('hello')
+        result = func(*args, **kwargs)
+        return result
+
+    return wrapper
+
+
+@greeting
+def person(name: str):
+    """
+    Print person's name
+    """
+    print(name)
+
+
+if __name__ == '__main__':
+    person('John')
+    print(person.__name__)
+    print(person.__doc__)
+    print(person.__annotations__)
