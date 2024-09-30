4993a4991876c66453affa5c93d31a3d05eb6c7a
RHA1705 <garbarzroman47@gmail.com>
2024-09-30 13:00:00 +0200

    module5_actualization

---
+++ WEB/module5/browser/conusmer.py
@@ -0,0 +1,19 @@
+import asyncio
+import logging
+import platform
+import websockets
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def consumer(hostname: str, port: int):
+    ws_resource_url = f"ws://{hostname}:{port}"
+    async with websockets.connect(ws_resource_url) as ws:
+        async for message in ws:
+            logging.info(f"Message: {message}")
+
+
+if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(consumer('localhost', 4000))
